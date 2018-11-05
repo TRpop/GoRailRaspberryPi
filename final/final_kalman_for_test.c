e3f1aa21e7215761a73e28e354d0b677777bc5c3
hyunjong <hyunjong96@gmail.com>
2018-11-05 01:59:09 +0000

    lagacy removed

--- final/final_kalman_for_test.c
+++ final/final_kalman_for_test.c
@@ -69,10 +69,7 @@ enum SystemState {OPERATING = 1, HEADING_FRONT = 2, HEADING_BACK = 4, NOT_OPERAT
 
 //////////////////////////////Functions//////////////////////////////
 void alarmWakeup(int sig_num);
-string make_string(int, int, vector<Point >&);
 string make_string(int from, int to, vector<DrivingData>& vec, char lr);
-//double PerpendicularDistance(const Point &pt, const Point &lineStart, const Point &lineEnd);
-//void RamerDouglasPeucker(const vector<Point> &pointList, double epsilon, vector<Point> &out);
 short read_raw_data(int fd, int addr);
 int _str2uuid( const char *uuid_str, uuid_t *uuid );
 sdp_session_t *register_service(uint8_t rfcomm_channel);
@@ -82,14 +79,9 @@ void write_server(int client, const char *message, int length);
 CarState decodeCarState(short state);
 double decodeDelta(int Encoded);
 void parse(char[], gps_data_t &data);
-//void WGS2UTM(float Latitude, float Longitude, float &lfUtmX, float &lfUtmY);
 void update_distance(int, Temperature, Temperature);
 void update_distance_back(int, DrivingData&);
-Point getmax(vector<Point>, int begin, int end, int &maxIndex);
 void getCheckPoint(vector<DrivingData> &save, vector<DrivingData> &right_check, vector<DrivingData> &left_check);
-void getCheckPoint(vector<Point> &save, vector<Point> &check);
-void sortByFirst(vector<Point> &object);
-void sortBySecond(vector<Point> &object);
 void init();
 bool isNear(DrivingData& a, DrivingData& b);
 bool isNear(vector<DrivingData>& a, DrivingData& b);
@@ -623,33 +615,6 @@ CarState decodeCarState(short state){
         }
 }
 
-string make_string(int from, int to, vector<Point >& vec){
-        string temp = "";
-        if(to > vec.size()) {
-                for(int i = from; i < vec.size(); i++) {
-                        string x = to_string(vec.at(i).first);
-                        string y = to_string(vec.at(i).second);
-                        x.erase ( x.find_last_not_of('0') + 1, string::npos );
-                        y.erase ( y.find_last_not_of('0') + 1, std::string::npos );
-                        temp += "x" + x + "y" + y;
-                }
-                temp += "x&*b\n";
-        }else{
-                for(int i = from; i < to; i++) {
-                        string x = to_string(vec.at(i).first);
-                        string y = to_string(vec.at(i).second);
-                        x.erase ( x.find_last_not_of('0') + 1, string::npos );
-                        y.erase ( y.find_last_not_of('0') + 1, std::string::npos );
-                        temp += "x" + x + "y" + y;
-                }
-                temp += "xb\n";
-        }
-
-        //temp += "x&*b\n";
-
-        return temp;
-}
-
 string make_string(int from, int to, vector<DrivingData>& vec, char lr){
         string temp = "";
 	if(lr == 'r'){
@@ -1041,43 +1006,6 @@ void parse(char line[], gps_data_t &data){
         }
 }
 
-/*
-   void WGS2UTM(float Latitude, float Longitude, float &lfUtmX, float &lfUtmY){
-   int iUTM_zone;
-   double dLat, dLon;
-   // coordinates in radians
-   dLat = Latitude*M_PI/180;
-   dLon = Longitude*M_PI/180;
-   // UTM parameters
-   double lon0_f = floor(Longitude/6)*6+3; // reference longitude in degrees
-   double lon0 = lon0_f*M_PI/180; // in radians
-   double k0 = 0.9996; // scale on central meridian
-   int FE = 500000; // false easting
-   int FN = (Latitude < 0)*10000000; // false northing
-   // Equations parameters
-   // N: radius of curvature of the earth perpendicular to meridian plane
-   // Also, distance from point to polar axis
-   double WN = Wa/sqrt( 1 - pow(We,2)*pow(sin(dLat),2));
-   double WT = pow(tan(dLat),2);
-   double WC = (pow(We,2)/(1 - pow(We,2)))*pow(cos(dLat),2);
-   double WLA = (dLon - lon0)*cos(dLat);
-   // M: true distance along the central meridian from the equator to lat
-   double WM = Wa*((1 - pow(We,2)/4 - 3*pow(We,4)/64 - 5*pow(We,6)/256)*dLat
-   - (3*pow(We,2)/8 + 3*pow(We,4)/32 + 45*pow(We,6)/1024)*sin(2*dLat)
- + (15*pow(We,4)/256 + 45*pow(We,6)/1024)*sin(4*dLat) - (35*pow(We,6)/3072)*sin(6*dLat));
-   // easting
-   lfUtmX = FE + k0*WN*(WLA + (1 - WT + WC)*pow(WLA,3)/6 + (5 - 18*WT + pow(WT,2) + 72*WC - 58*Weps)*pow(WLA,5)/120);
-   // northing
-   // M(lat0) = 0 so not used in following formula
-   lfUtmY = FN + k0*WM + k0*WN*tan(dLat)*(pow(WLA,2)/2 + (5 - WT + 9*WC + 4*pow(WC,2))*pow(WLA,4)/24
- + (61 - 58*WT + pow(WT,2) + 600*WC - 330*Weps)*pow(WLA,6)/720);
-   // UTM zone
-   //iZone = (int)(floor(lon0_f/6)+31);
-   //cout<<"UTM_X : "<<dUTM_X<<" / ";
-   //cout<<"UTM_Y : "<<dUTM_Y<<endl;
-   }
- */
-
 void update_distance(int encoder, Temperature left, Temperature right){
 	utm_data_t utm_data;
 	UTM::LLtoUTM(gps_data.latitude, gps_data.longitude, utm_data.y, utm_data.x, utm_data.zone);
@@ -1160,22 +1088,6 @@ void update_distance_back(int encoder, DrivingData& current){
 	printf("down = [down; %f %f %f];\n", current.ll.lat, current.ll.lng, X(2, 0));
 }
 
-Point getmax(vector<Point> vec, int begin, int end, int &maxIndex){
-        vector<Point>::iterator it;
-        double max = -10000;
-        Point maxPoint;
-        int n = 0;
-        for(it = vec.begin() + begin; it != vec.begin() + end; it++, n++) {
-                if(max < it->second) {
-                        max = it->second;
-                        maxPoint = *it;
-                        maxIndex = begin + n;
-                }
-        }
-
-        return maxPoint;
-}
-
 void getmax(vector<DrivingData> &vec, int begin, int end, int &maxIndexr, int &maxIndexl){
         vector<DrivingData>::iterator it;
         double rmax = -10000, lmax = -10000;
@@ -1226,41 +1138,6 @@ void getCheckPoint(vector<DrivingData> &save, vector<DrivingData> &right_check, 
 			left_check.push_back(save.at(lmax));
 		}
 	}
-
-        /*
-	int rlast = 0, llast = 0;
-        getmax(save, 0, K_SIZE, rlast, llast);
-
-	right_check.push_back(save.at(rlast));
-
-        for(int i = K_SIZE; i < save.size(); i++) {
-                if((i - rlast) < K_SIZE) {
-                        if(right_check.back().r_temp < save.at(i).r_temp) {
-                                right_check.pop_back();
-                                right_check.push_back(save.at(i));
-                                rlast = i;
-                        }
-                }else{
-                        right_check.push_back(save.at(i));
-                        rlast = i;
-                }
-        }
-
-	left_check.push_back(save.at(llast));
-
-        for(int i = K_SIZE; i < save.size(); i++) {
-                if((i - llast) < K_SIZE) {
-                        if(left_check.back().l_temp < save.at(i).l_temp) {
-                                left_check.pop_back();
-                                left_check.push_back(save.at(i));
-                                llast = i;
-                        }
-                }else{
-                        left_check.push_back(save.at(i));
-                        llast = i;
-                }
-        }
-	*/
 }
 
 void init(){
@@ -1293,24 +1170,8 @@ void init(){
 	travelDistance = 0;
 }
 
-void sortByFirst(vector<Point> &object){
-        sort(object.begin(), object.end(), [] (Point const& a, Point const& b) { return a.first < b.first; });
-}
-
-void sortBySecond(vector<Point> &object){
-        sort(object.begin(), object.end(), [] (Point const& a, Point const& b) { return a.second < b.second; });
-}
-
 void sortByDistance(vector<DrivingData> &object){
 	sort(object.begin(), object.end(), [] (DrivingData const& a, DrivingData const& b) { return a.distance < b.distance; });
-}
-
-void sortByRightTemparature(vector<DrivingData> &object){
-	sort(object.begin(), object.end(), [] (DrivingData const& a, DrivingData const& b) { return a.r_temp < b.r_temp; });
-}
-
-void sortByLeftTemparature(vector<DrivingData> &object){
-        sort(object.begin(), object.end(), [] (DrivingData const& a, DrivingData const& b) { return a.l_temp < b.l_temp; });
 }
 
 void sortByTemparature(vector<DrivingData> &right, vector<DrivingData> &left){
